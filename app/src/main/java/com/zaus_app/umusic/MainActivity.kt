a908adde3f4187c86e072623ed035f38c42608fd
KrokodilGenadiy <alex.usevich@gmail.com>
2022-04-10 19:33:49 +0300

    set drawer navigation enabled viewbinding added some folder for MVVM

--- app/src/main/java/com/zaus_app/umusic/MainActivity.kt
+++ app/src/main/java/com/zaus_app/umusic/MainActivity.kt
@@ -2,10 +2,42 @@ package com.zaus_app.umusic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.zaus_app.umusic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+     private val toggle: ActionBarDrawerToggle by lazy {
+         ActionBarDrawerToggle(this,binding.drawerLayout,R.string.open,R.string.close)
+     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.item1 -> Toast.makeText(applicationContext,"Clicked Item 1",Toast.LENGTH_SHORT).show()
+                R.id.item2 -> Toast.makeText(applicationContext,"Clicked Item 2",Toast.LENGTH_SHORT).show()
+                R.id.item3 -> Toast.makeText(applicationContext,"Clicked Item 3",Toast.LENGTH_SHORT).show()
+            }
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
